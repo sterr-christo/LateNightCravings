7079cc675fc93a2550cc6a32277692ec07485e42
c <sterr.christo@gmail.com>
2016-11-14 10:48:09 -0600

    first commit

---
+++ build/classes/test.js
@@ -0,0 +1,27 @@
+var txtUsername = document.getElementById("username");
+var txtPassword = document.getElementById("password");
+var chkRemember = document.getElementById("rememberMe");
+var btnLogin = document.getElementById("login");
+var btnSearch = document.getElementById("btnSearch");
+var txtSearch = document.getElementById("searchbox");
+
+//btnSubmit.addEventListener("click",submit);
+//studentID.addEventListener("blur",isNumeric);
+
+
+function isNumeric(elem){
+	var numericExpression = /^[0-9]+$/;
+	if(elem.value.match(numericExpression)){
+		return true;
+	}else{
+	    elme.value = '';
+		elem.focus();
+		return false;
+	}
+}
+
+function restrictLength(element,max_chars) {
+    if(element.value.length > max_chars) {
+        element.value = element.value.substr(0,max_chars);
+    }
+}
---
+++ src/createTables.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Restaurant(RestaurantID INT PRIMARY KEY, Name TEXT, Zip INT, State TEXT, City TEXT, Street TEXT, Latitude INT, Longitude INT, Delivery NUMERIC, Phone TEXT, Website TEXT, Email TEXT, ClosingTime INT);
+
+CREATE TABLE Items(ItemID INT PRIMARY KEY, Name TEXT, Price, REAL, Description TEXT);
+
+CREATE TABLE Serves(ItemID INT REFERENCES Item(ItemID), RestaurantID INT REFERENCES Restaurant(RestaurantID), PRIMARY KEY(ItemID, RestaurantID));
+
+CREATE TABLE Genre(GenreID INT PRIMARY KEY, RestaurantID INT NOT NULL REFERENCES Restaurant(RestaurantID), Name TEXT, Description TEXT);
+
+CREATE TABLE User(Username TEXT PRIMARY KEY, Password TEXT NOT NULL, Zip INT, State TEXT, City TEXT, Street TEXT, Latitude INT, Longitude INT);
+
+CREATE TABLE Review(RestaurantID INT REFERENCES Restaurant(RestaurantID) NOT NULL, Username TEXT REFERENCES User(Username) NOT NULL, Comments TEXT, Rating INT, PRIMARY KEY(RestaurantID, Username));
