7a2b1421930f0ba65a95845ddfa776c17247d678
salvatormatt <salvator.matt@uwlax.edu>
2016-12-03 21:40:03 -0600

    Marcus' latest work
    The commented parts are part of the picture. Don't worry about their
    implementation for now.

--- src/User.java
+++ src/User.java
@@ -1,16 +1,42 @@
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class User {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		JFrame j = new JFrame();
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/*
+		j.setContentPane(new JPanel() {
+	      
+	       	BufferedImage image = ImageIO.read(new File("FAST_FOOD.jpg"));
+	        public void paintComponent(Graphics g) {
+	            super.paintComponent(g);
+	            g.drawImage(image, 0, 0, 500, 500, this);
+	        }
+	    });
+	    */
+	    
+		
 		JPanel p = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		c.gridwidth=1;
 		c.gridheight=1;	
-		c.fill = GridBagConstraints.CENTER	;
+		c.fill = GridBagConstraints.HORIZONTAL;
+		j.pack();
+		//p.setBackground( Color.white );
+		
+		
+		//BufferedImage myImage = ImageIO.read( new File( "FAST_FOOD.jpg"));
+		//JFrame myJFrame = new JFrame("Image pane");
+		//j.setContentPane(new ImagePanel(myImage));
+		
+		
 		
 		j.setTitle("Stoner's Late Night Cravings - User Panel");
 		j.setVisible(true);
@@ -19,14 +45,15 @@ public class User {
 		j.add(p);
 		
 		JTextField username = new JTextField();
-
+		c.weightx = 1;
 		c.gridx=1;
 		c.gridy=0;
 
 		p.add(username,c);
 		
 		JTextField password = new JTextField();
-
+		
+		c.weightx = 1;
 		c.gridx=1;
 		c.gridy=1;
 
@@ -34,26 +61,32 @@ public class User {
 		
 		JButton login = new JButton("Login");
 
-		c.gridx=1;
+		c.gridwidth = 2;
+		c.gridx=0;
 		c.gridy=3;
 
 		p.add(login,c);
 		
 		JButton register = new JButton("Register");
 
-		c.gridx=1;
+		c.gridwidth = 2;
+		c.gridx=0;
 		c.gridy=4;
 
 		p.add(register,c);
 		
-		JLabel labUsername = new JLabel("User:");
-
+		JLabel labUsername = new JLabel("Username:");
+		
+		c.gridwidth = 1;
+		c.weightx = 0;
 		c.gridx=0;
 		c.gridy=0;
 
 		p.add(labUsername,c);
 		
-		JLabel labPassword = new JLabel("Pass:");
+		JLabel labPassword = new JLabel("Password:");
+		c.gridwidth = 1;
+		c.weightx = 0;
 		c.gridx=0;
 		c.gridy=1;
 
@@ -69,3 +102,17 @@ public class User {
 		
 	}
 }
+
+
+/*class ImagePanel extends JComponent {
+    private Image image;
+    public ImagePanel(Image image) {
+        this.image = image;
+    }
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(image, 0, 0, this);
+    }
+}
+*/
