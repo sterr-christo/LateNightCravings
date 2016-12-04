cb6852f2e2cf0b9b435c608ee961767a0d6b4f06
Unknown <sterr.christo@gmail.com>
2016-12-03 20:42:37 -0600

    stuff
    123

--- src/User.java
+++ src/User.java
@@ -8,10 +8,9 @@ public class User {
 		JFrame j = new JFrame();
 		JPanel p = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
-		c.gridwidth=2;
-		c.gridheight=5;
-		c.insets = new Insets(0,0,0,0);
-		
+		c.gridwidth=1;
+		c.gridheight=1;	
+		c.fill = GridBagConstraints.CENTER	;
 		
 		j.setTitle("Stoner's Late Night Cravings - User Panel");
 		j.setVisible(true);
@@ -20,42 +19,46 @@ public class User {
 		j.add(p);
 		
 		JTextField username = new JTextField();
-		username.setBounds(40, 15, (int)(j.getWidth()*.80), 14);
+
 		c.gridx=1;
 		c.gridy=0;
+
 		p.add(username,c);
 		
 		JTextField password = new JTextField();
-		password.setBounds(40, 35, (int)(j.getWidth()*.80), 14);
+
 		c.gridx=1;
 		c.gridy=1;
+
 		p.add(password,c);
 		
-		JButton login = new JButton();
-		login.setBounds(60 ,51, (int)(j.getWidth()*.5), 16);
+		JButton login = new JButton("Login");
+
 		c.gridx=1;
 		c.gridy=3;
+
 		p.add(login,c);
 		
-		JButton register = new JButton();
-		register.setBounds(20 ,70, (int)(j.getWidth()*.8), 16);
+		JButton register = new JButton("Register");
+
 		c.gridx=1;
 		c.gridy=4;
+
 		p.add(register,c);
 		
-		JLabel labUsername = new JLabel();
-		labUsername.setText("User:");
-		labUsername.setBounds(5, 15, labUsername.getWidth(), labUsername.getHeight());
+		JLabel labUsername = new JLabel("User:");
+
 		c.gridx=0;
 		c.gridy=0;
+
 		p.add(labUsername,c);
 		
-		JLabel labPassword = new JLabel();
-		labUsername.setText("Pass:");
-		labUsername.setBounds(5, 15, labUsername.getWidth(), labUsername.getHeight());
+		JLabel labPassword = new JLabel("Pass:");
 		c.gridx=0;
 		c.gridy=1;
+
 		p.add(labPassword,c);
+		
 		
 		p.repaint();
 		
